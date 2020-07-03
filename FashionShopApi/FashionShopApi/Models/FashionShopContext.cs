b7b09b2b39a062a0b5d2993ed56132b06d8caf5b
xuanhapham <phamthixuanha@secure.janeto.com>
2020-07-03 17:00:58 +0700

    API

---
+++ FashionShopApi/FashionShopApi/Models/FashionShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FashionShopApi.Models
+{
+    public class FashionShopContext : DbContext
+    {
+        public FashionShopContext(DbContextOptions<FashionShopContext> options) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
